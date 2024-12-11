cde7a810c228942f9d2b94dcea623fd0e971bfcc
viswanath Reddy <viswanath3344@gmail.com>
2024-12-11 16:46:39 +0530

    Added user schema pre save method to password salting and compare method to check the password comparision

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -9,17 +9,49 @@ const router = express.Router();
 router.post("/signup", async (req, res) => {
     const { email, password } = req.body;
     try {
-    const user = new User({ email, password });
-    await user.save()
-    const token = JWT.sign({id: user._id}, "MY_SECRET_KEY");
-    res.send({
-        message: "Registration Success", 
-        token
-    });
+        const user = new User({ email, password });
+        await user.save()
+        const token = JWT.sign({ id: user._id }, "MY_SECRET_KEY");
+        res.send({
+            message: "Registration Success",
+            token
+        });
 
-    }catch (err) {
-      res.status(422).send({message: "Registration failed"})
+    } catch (err) {
+        res.status(422).send({ message: "Registration failed" })
     }
+})
+
+
+router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).send({ message: "Must provide email and password" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(422).send({ message: "Invalid username" })
+    }
+
+    try {
+        const isMatched = await user.comparePassword(password);
+
+        console.log("isMatched", isMatched);
+        if (!isMatched) {
+            return res.status(422).send({ message: "Invalid credentials" })
+        }
+
+        const token = JWT.sign({ id: user._id }, "MY_SECRET_KEY");
+
+       return res.status(200).send({ message: "Login success", token })
+
+    } catch (err) {
+        return res.status(422).send({ message: "Invalid Credentials" })
+    }
+
 })
 
 module.exports = router;
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const user = new mongoose.Schema({
     email : {
@@ -11,5 +12,46 @@ const user = new mongoose.Schema({
         required: true
     }
 })
+
+user.pre("save", function(next) {
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }  
+
+    bcrypt.genSalt(10, (err, salt)=> {
+        if(err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, (err, hashPassword) => {
+            if(err) {
+                return next(err);
+            }
+    
+            user.password = hashPassword;
+            next();
+        });
+    })
+
+})
+
+user.methods.comparePassword = function(candidatePassword) {
+    const user = this
+
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatched) => {
+            if(err) {
+                return reject(err);
+            }
+
+            if(!isMatched) {
+                return reject(false);
+            }
+
+            resolve(true);
+        })
+    })
+}
 
 mongoose.model("User", user);
