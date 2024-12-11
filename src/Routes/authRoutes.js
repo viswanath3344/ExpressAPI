187daf1fc13826b3eab6531f9990cf909196d47a
viswanath Reddy <viswanath3344@gmail.com>
2024-12-11 12:25:54 +0530

    Added JWT token, created token to send it

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = mongoose.model('User');
+const JWT = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -9,7 +10,11 @@ router.post("/signup", async (req, res) => {
     try {
     const user = new User({ email, password });
     await user.save()
-    res.status(200).send({message: "Registration Success"})
+    const token = JWT.sign({id: user._id}, "MY_SECRET_KEY");
+    res.send({
+        message: "Registration Success", 
+        token
+    });
     }catch (err) {
       res.status(422).send({message: "Registration failed"})
     }
